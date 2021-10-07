afce4a0d0d6515c6a9c41ca38848a16d6f48517e
lilly3ka <92005057+lilly3ka@users.noreply.github.com>
2021-10-07 14:47:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    introTitle: 'Zarlinkk đây rùi😍',
+    introDesc: `Hap pi birthday 1 lần nữaa🥳🤩
+    Mong năm nay bà sẽ có thật nhiều
+    Thật nhiềuuuu
+    kỉ niệm zui zẻ nha🎂💖`,
+    btnIntro: 'nhấn zô đây i',
+    title: 'Quiz nhỏ khảo sát tình bạn ',
+    desc: 'Gia Linh sẽ..',
+    btnYes: 'Đi với Tường Uyên!!!',
+    btnNo: 'Đi coi bươm bướm với anh SK ',
+    question:'Đúng rùi phải chọn tui chớ😎Lý do bà làm bạn vói tui🥺',
+    btnReply: 'Từ siêu sao zũ trụ gửi tới Tường Uyên',
+    reply: 'Tại mình hợp nhau quá chứ gì nữaaaa',
+    mess: 'Tui biết mà 🥰. Rùi câu cuối nè',
+    messDesc: 'sisters 4ver?',
+    btnAccept: 'OF COURSE!!',
+    messLink: 'https://www.facebook.com/messages/t/100011067134826' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
